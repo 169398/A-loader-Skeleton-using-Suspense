189f62d182902a0dd5ea90911f9a96d5b41b56ce
idris kulubi <137810838+169398@users.noreply.github.com>
2024-04-21 23:13:14 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
 <Button>
-  Sample text
+  Sample text to render the  shadcn button
 </Button>
       </div>
     </main>
